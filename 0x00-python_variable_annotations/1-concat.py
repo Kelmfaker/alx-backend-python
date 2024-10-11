4f63f005b30ee5667b8f1e18cba735724257ef68
Kelmfaker <kelmfaker@gmail.com>
2024-10-11 00:25:49 +0000

    concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""
+a type-annotated function concat
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+
+    """
+    return concatenated string
+    """
+    return str1 + str2
